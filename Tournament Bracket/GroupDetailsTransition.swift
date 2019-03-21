972c8af803eda4bb74050ee2746e14a33efe0f0d
Konstantin Ryzhikh <kryzhikh@distillery.com>
2019-03-21 14:33:37 +0300

    transition

--- Tournament Bracket/GroupDetailsTransition.swift
+++ Tournament Bracket/GroupDetailsTransition.swift
@@ -35,16 +35,22 @@ class GroupDetailsTransition: NSObject, UIViewControllerAnimatedTransitioning {
             let fromController = transitionContext.viewController(forKey: .from) as! ViewController
             toController.view.frame = transitionContext.finalFrame(for: toController)
             toController.groupContainerView.alpha = 0
+            containerView.backgroundColor = fromController.view.backgroundColor
+            
 //            toController.contentContainer.alpha = 0
 //            toController.contentContainer.transform = CGAffineTransform(translationX: 0, y: -150)
+            
             let ip = toController.indexPath
             let targetCell = fromController.transitionCollectionView.cellForItem(at: ip)!
             let snapshot = targetCell.snapshotView(afterScreenUpdates: false)!
             let snapshotOrigin = fromController.groupsCollectionView.convert(targetCell.frame.origin, to: containerView)
             snapshot.frame = CGRect(origin: snapshotOrigin, size: targetCell.frame.size)
+            
             containerView.addSubview(fromController.view)
             containerView.addSubview(toController.view)
+            toController.view.alpha = 0
             containerView.addSubview(snapshot)
+            
             targetCell.alpha = 0
             let toOrigin = toController.groupContainerView.superview!.convert(toController.groupContainerView.frame.origin, to: containerView)
             
@@ -52,17 +58,20 @@ class GroupDetailsTransition: NSObject, UIViewControllerAnimatedTransitioning {
             anim.fromValue = -50
             anim.toValue = 0
             anim.duration = duration
+//            anim.speed = 0.8
             anim.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
             let reveal = CABasicAnimation(keyPath: "opacity")
             reveal.fromValue = 0.0
             reveal.toValue = 1.0
             reveal.duration = duration
+//            reveal.speed = 1
             toController.contentContainer.layer.add(anim, forKey: nil)
             toController.contentContainer.layer.add(reveal, forKey: nil)
             
             UIView.animate(withDuration: duration, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
                 snapshot.frame.origin.y = toOrigin.y
-                fromController.view.alpha = 0
+//                fromController.view.alpha = 0
+                toController.view.alpha = 1
 //                toController.contentContainer.alpha = 1
 //                toController.contentContainer.transform = CGAffineTransform.identity
 //                fromController.groupsCollectionView.visibleCells.forEach({ cell in
